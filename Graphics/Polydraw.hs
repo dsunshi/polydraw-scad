2964d6b7aae0ae98d331c64999b57c546398ad0b
D. Sunshine <david@sunshines.org>
2024-02-24 01:10:08 -0500

    Render for V2 and V3
    Simple render function for V2 and V3.

--- Graphics/Polydraw.hs
+++ Graphics/Polydraw.hs
@@ -1,3 +1,28 @@
+{-# LANGUAGE FlexibleInstances #-}
 
+ module Graphics.Polydraw where
+
+import Text.Printf
 import Linear.V2
 import Linear.V3
+
+-- Common class for V2 and V3
+class Vector a where
+    render :: a -> String
+
+instance Vector (V2 Float) where
+    render (V2 x y) = printf "[%.3f, %.3f]" x y
+
+instance Vector (V2 Double) where
+    render (V2 x y) = printf "[%.3f, %.3f]" x y
+
+instance Vector (V2 Integer) where
+    render (V2 x y) = printf "[%d, %d]" x y
+
+instance (Num a, PrintfArg a) => Vector (V3 a) where
+    render (V3 x y z) = printf "[%.3f, %.3f, %.3f]" x y z
+
+data Solid a =
+    Cube a
+  | Cylinder a a
+    deriving (Eq, Ord, Show, Read)
