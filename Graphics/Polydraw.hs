8e057042308aef5cc317c1d4fdfe5a5659971799
D. Sunshine <david@sunshines.org>
2024-02-27 05:40:20 -0500

    Clean up
    1. Single number type
    2. renderDouble for pretty output printing
    3. Improved code cleanup

--- Graphics/Polydraw.hs
+++ Graphics/Polydraw.hs
@@ -6,40 +6,32 @@ import Text.Printf
 import Linear.V2
 import Linear.V3
 
-data Numm = Float | Double | Integer
+-- Returns if x is an int to n decimal places
+isInt :: (Integral a, RealFrac b) => a -> b -> Bool
+isInt n x = round (10 ^ fromIntegral n * (x - fromIntegral (round x))) == 0
+
+renderDouble :: Double -> String
+renderDouble n
+  | isInt precision n = printf "%d" (floor n :: Int)
+  | otherwise         = printf ("%." ++ show precision ++ "f") n
+    where precision   = 3
+
+fromV2 :: V2 Double -> V3 Double
+fromV2 (V2 x y) = V3 x y 0
 
 -- Common class for V2 and V3
 class Vector a where
     renderVector :: a -> String
 
-instance Vector (V2 Rational) where
-    renderVector (V2 x y) = printf "[%s, %s]" (show x) (show y)
-
-instance Vector (V2 Float) where
-    renderVector (V2 x y) = printf "[%.3f, %.3f]" x y
-
 instance Vector (V2 Double) where
-    renderVector (V2 x y) = printf "[%.3f, %.3f]" x y
-
-instance Vector (V2 Integer) where
-    renderVector (V2 x y) = printf "[%d, %d]" x y
-
-instance Vector (V3 Rational) where
-    renderVector (V3 x y z) = printf "[%s, %s, %s]" (show x) (show y) (show z)
-
-instance Vector (V3 Float) where
-    renderVector (V3 x y z) = printf "[%.3f, %.3f, %.3f]" x y z
+    renderVector (V2 x y) = printf "[%s, %s]" (renderDouble x) (renderDouble y)
 
 instance Vector (V3 Double) where
-    renderVector (V3 x y z) = printf "[%.3f, %.3f, %.3f]" x y z
-
-instance Vector (V3 Integer) where
-    renderVector (V3 x y z) = printf "[%d, %d, %d]" x y z
-
+    renderVector (V3 x y z) = printf "[%s, %s, %s]" (renderDouble x) (renderDouble y) (renderDouble z)
 
 data Solid =
-    Cube Rational
-  | Cylinder Rational Rational
+    Cube Double
+  | Cylinder Double Double
     deriving Show
 
 data Model m =
@@ -47,21 +39,21 @@ data Model m =
   | Translate m (Model m)
     deriving Show
 
-type Model3d = Model (V3 Rational)
+type Model3d = Model (V3 Double)
 
-cube :: Rational -> Model3d
-cube = Solid . Cube
+cube :: Double -> Model3d
+cube s = translate (fromV2 $ V2 (-s/2) (-s/2) ) (Solid $ Cube s)
 
 translate :: Vector v => v -> Model v -> Model v
 translate = Translate
 
 renderModel :: Vector v => Model v -> String
-renderModel (Translate v s) = renderVectorSolid "translate" v s
-renderModel (Solid s) = renderSolid s
+renderModel (Translate v s) = renderTransform "translate" v s
+renderModel (Solid s)       = renderSolid s
 
-renderVectorSolid :: (Vector a, Vector m) => String -> a -> Model m -> String
-renderVectorSolid name vec solid = printf "%s(%s) %s" name (renderVector vec) (renderModel solid)
+renderTransform :: (Vector a, Vector m) => String -> a -> Model m -> String
+renderTransform tName v model = printf "%s(%s) %s" tName (renderVector v) (renderModel model)
 
 renderSolid :: Solid -> String
 renderSolid (Cube s) = printf "cube(%s);\n" (show s)
-renderSolid _ = undefined
+renderSolid _        = undefined
